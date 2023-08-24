ccbbe5c773a1de448d89a3c7f2421a6b16669ba5
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-08-25 02:14:02 +0700

    improve: add component

---
+++ src/components/molecules/inputs/files/single/index.tsx
@@ -0,0 +1,21 @@
+import { FC, ReactElement } from "react";
+
+export {};
+
+export const SingleUploadField: FC = (): ReactElement => {
+  return (
+    <div className="flex flex-col gap-y-2">
+      <label
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        htmlFor="file_input"
+      >
+        Upload file
+      </label>
+      <input
+        className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+        id="file_input"
+        type="file"
+      />
+    </div>
+  );
+};
---
+++ src/components/molecules/inputs/index.ts
@@ -0,0 +1,6 @@
+export * from "./text";
+export * from "./select";
+export * from "./select";
+export * from "./checkbox";
+export * from "./radio";
+export * from "./files";
