f99e6d7d9332b23966a013c9d8fa62d827109c98
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-08-25 02:19:18 +0700

    improve: add new mock mechanism

--- src/modules/books/hook.ts
+++ src/modules/books/hook.ts
@@ -1,12 +1,12 @@
 import { TBookResponse, TMetaErrorResponse } from "@/entities";
 import { UseQueryResult, useQuery } from "@tanstack/react-query";
-import { getMockBooks, getBooks } from "./api";
+import { getBooks } from "./api";
 
 export const useGetBooks = (
   type: "mock" | "api",
 ): UseQueryResult<TBookResponse, TMetaErrorResponse> => {
   return useQuery({
     queryKey: ["books", type],
-    queryFn: async () => await (type === "mock" ? getMockBooks() : getBooks()),
+    queryFn: async () => await getBooks(type),
   });
 };
