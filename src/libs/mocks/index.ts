a43a94ae7e441c400280ce7948f81e30eeda158c
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-08-25 01:30:46 +0700

    improve/fix-conflict

--- src/libs/mocks/index.ts
+++ src/libs/mocks/index.ts
@@ -16,14 +16,7 @@ export function makeServer({ environment = "test" }): void {
 
   serverState.logging = true;
 
-<<<<<<< HEAD
-  serverState.post(
-    `${import.meta.env.VITE_API_URL}/:any`,
-    () => new Promise((): void => {}),
-  );
-=======
   serverState.post(`${import.meta.env.VITE_API_URL}/:any`, () => new Promise((): void => {}));
->>>>>>> 0120e94 (improve: add new params)
 
   serverState.urlPrefix = import.meta.env.API_PREFIX ?? "/api";
 
